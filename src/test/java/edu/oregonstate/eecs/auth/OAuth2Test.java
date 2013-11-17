6e4af19df04d156a7b96b74be34239f06e570f2a
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2013-11-16 17:46:26 -0800

    initial commit

---
+++ src/test/java/edu/oregonstate/eecs/auth/OAuth2Test.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.eecs.auth;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OAuth2Test {
+	@Test
+	public void testSomething() {
+		Assert.assertEquals(1, 1);
+	}
+}
